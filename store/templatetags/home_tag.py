0e3773c8acda24643dc3ab505f2f5ad5003a4101
redmanout <vladimirkotov1115@gmail.com>
2023-02-08 17:32:58 +0200

    made the output of product filters on pages in simple tags

--- store/templatetags/home_tag.py
+++ store/templatetags/home_tag.py
@@ -1,5 +1,11 @@
 from django import template
-from store.models import Product
+from store.models import CategoryProduct, BladesProductAttribute, BladesBrand, BladesType, BladesHandleType, \
+    BladesComposition, BladesSize, RubbersProductAttribute, RubbersBrand, RubbersType, RubbersSpeedType, \
+    BallsProductAttribute, BallsBrand, BallsRank, BallsPackage, BackpacksBagsProductAttribute, BackpacksBagsBrand, \
+    BackpacksBagsType, BackpacksBagsColor, NetsColor, NetsBrand, NetsProductAttribute, TablesProductAttribute, \
+    TablesBrand, TablesColor, TablesSection, TablesThickness, RacketsProductAttribute, RacketsBrand, RacketsType, \
+    RacketsHandleType, RacketsAverageWeight, RacketsRubbersThickness, AccessoriesProductAttribute, AccessoriesBrand, \
+    AccessoriesType, AccessoriesColor
 
 register = template.Library()
 
@@ -12,3 +18,188 @@ def get_phone_contacts():
         '+38(098)980-96-23',
     ]
     return phone_numbers_contacts
+
+
+@register.simple_tag()
+def get_categories():
+    return CategoryProduct.objects.all()
+
+
+@register.simple_tag()
+def get_attribute_blades():
+    return BladesProductAttribute.objects.all()
+
+
+@register.simple_tag()
+def get_brand_blades():
+    return BladesBrand.objects.all()
+
+
+@register.simple_tag()
+def get_type_blades():
+    return BladesType.objects.all()
+
+
+@register.simple_tag()
+def get_handle_type_blades():
+    return BladesHandleType.objects.all()
+
+
+@register.simple_tag()
+def get_composition_blades():
+    return BladesComposition.objects.all()
+
+
+@register.simple_tag()
+def get_size_blades():
+    return BladesSize.objects.all()
+
+
+@register.simple_tag()
+def get_attribute_rubbers():
+    return RubbersProductAttribute.objects.all()
+
+
+@register.simple_tag()
+def get_brand_rubbers():
+    return RubbersBrand.objects.all()
+
+
+@register.simple_tag()
+def get_type_rubbers():
+    return RubbersType.objects.all()
+
+
+@register.simple_tag()
+def get_speed_type_rubbers():
+    return RubbersSpeedType.objects.all()
+
+
+@register.simple_tag()
+def get_attribute_balls():
+    return BallsProductAttribute.objects.all()
+
+
+@register.simple_tag()
+def get_brand_balls():
+    return BallsBrand.objects.all()
+
+
+@register.simple_tag()
+def get_rank_balls():
+    return BallsRank.objects.all()
+
+
+@register.simple_tag()
+def get_package_balls():
+    return BallsPackage.objects.all()
+
+
+@register.simple_tag()
+def get_attribute_backpacksbags():
+    return BackpacksBagsProductAttribute.objects.all()
+
+
+@register.simple_tag()
+def get_brand_backpacksbags():
+    return BackpacksBagsBrand.objects.all()
+
+
+@register.simple_tag()
+def get_type_backpacksbags():
+    return BackpacksBagsType.objects.all()
+
+
+@register.simple_tag()
+def get_color_backpacksbags():
+    return BackpacksBagsColor.objects.all()
+
+
+@register.simple_tag()
+def get_attribute_nets():
+    return NetsProductAttribute.objects.all()
+
+
+@register.simple_tag()
+def get_brand_nets():
+    return NetsBrand.objects.all()
+
+
+@register.simple_tag()
+def get_color_nets():
+    return NetsColor.objects.all()
+
+
+@register.simple_tag()
+def get_attribute_tables():
+    return TablesProductAttribute.objects.all()
+
+
+@register.simple_tag()
+def get_brand_tables():
+    return TablesBrand.objects.all()
+
+
+@register.simple_tag()
+def get_color_tables():
+    return TablesColor.objects.all()
+
+
+@register.simple_tag()
+def get_section_tables():
+    return TablesSection.objects.all()
+
+
+@register.simple_tag()
+def get_thickness_tables():
+    return TablesThickness.objects.all()
+
+
+@register.simple_tag()
+def get_attribute_rackets():
+    return RacketsProductAttribute.objects.all()
+
+
+@register.simple_tag()
+def get_brand_rackets():
+    return RacketsBrand.objects.all()
+
+
+@register.simple_tag()
+def get_type_rackets():
+    return RacketsType.objects.all()
+
+
+@register.simple_tag()
+def get_handle_type_rackets():
+    return RacketsHandleType.objects.all()
+
+
+@register.simple_tag()
+def get_average_weight_rackets():
+    return RacketsAverageWeight.objects.all()
+
+
+@register.simple_tag()
+def get_rubbers_thickness_rackets():
+    return RacketsRubbersThickness.objects.all()
+
+
+@register.simple_tag()
+def get_attribute_accessories():
+    return AccessoriesProductAttribute.objects.all()
+
+
+@register.simple_tag()
+def get_brand_accessories():
+    return AccessoriesBrand.objects.all()
+
+
+@register.simple_tag()
+def get_type_accessories():
+    return AccessoriesType.objects.all()
+
+
+@register.simple_tag()
+def get_color_accessories():
+    return AccessoriesColor.objects.all()
--- store/views.py
+++ store/views.py
@@ -1,155 +1,14 @@
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import FormMixin
-from django.views.generic.base import View
-
 from .forms import ReviewForm
-from .models import BladesProductAttribute, BladesBrand, BladesType, BladesHandleType, BladesComposition, BladesSize, \
-    RubbersProductAttribute, RubbersBrand, RubbersType, RubbersSpeedType, BallsProductAttribute, BallsBrand, BallsRank, \
-    BallsPackage, BackpacksBagsProductAttribute, BackpacksBagsBrand, BackpacksBagsType, BackpacksBagsColor, \
-    NetsProductAttribute, NetsBrand, NetsColor, TablesProductAttribute, TablesBrand, TablesColor, TablesSection, \
-    TablesThickness, RacketsProductAttribute, RacketsBrand, RacketsType, RacketsHandleType, RacketsAverageWeight, \
-    RacketsRubbersThickness, AccessoriesProductAttribute, AccessoriesBrand, AccessoriesType, AccessoriesColor, \
-    CategoryProduct, Product
+from .models import CategoryProduct, Product
 
 
 # Create your views here.
 
-class BladesAttributeProduct:
-    def get_attribute_blades(self):
-        return BladesProductAttribute.objects.all()
 
-    def get_brand_blades(self):
-        return BladesBrand.objects.all()
-
-    def get_type_blades(self):
-        return BladesType.objects.all()
-
-    def get_handle_type_blades(self):
-        return BladesHandleType.objects.all()
-
-    def get_composition_blades(self):
-        return BladesComposition.objects.all()
-
-    def get_size_blades(self):
-        return BladesSize.objects.all()
-
-
-class RubbersAttributeProduct:
-    def get_attribute_rubbers(self):
-        return RubbersProductAttribute.objects.all()
-
-    def get_brand_rubbers(self):
-        return RubbersBrand.objects.all()
-
-    def get_type_rubbers(self):
-        return RubbersType.objects.all()
-
-    def get_speed_type_rubbers(self):
-        return RubbersSpeedType.objects.all()
-
-
-class BallsAttributeProduct:
-    def get_attribute_balls(self):
-        return BallsProductAttribute.objects.all()
-
-    def get_brand_balls(self):
-        return BallsBrand.objects.all()
-
-    def get_rank_balls(self):
-        return BallsRank.objects.all()
-
-    def get_package_balls(self):
-        return BallsPackage.objects.all()
-
-
-class BackpacksBagsAttributeProduct:
-    def get_attribute_backpacksbags(self):
-        return BackpacksBagsProductAttribute.objects.all()
-
-    def get_brand_backpacksbags(self):
-        return BackpacksBagsBrand.objects.all()
-
-    def get_type_backpacksbags(self):
-        return BackpacksBagsType.objects.all()
-
-    def get_color_backpacksbags(self):
-        return BackpacksBagsColor.objects.all()
-
-
-class NetsAttributeProduct:
-    def get_attribute_nets(self):
-        return NetsProductAttribute.objects.all()
-
-    def get_brand_nets(self):
-        return NetsBrand.objects.all()
-
-    def get_color_nets(self):
-        return NetsColor.objects.all()
-
-
-class TablesAttributeProduct:
-    def get_attribute_tables(self):
-        return TablesProductAttribute.objects.all()
-
-    def get_brand_tables(self):
-        return TablesBrand.objects.all()
-
-    def get_color_tables(self):
-        return TablesColor.objects.all()
-
-    def get_section_tables(self):
-        return TablesSection.objects.all()
-
-    def get_thickness_tables(self):
-        return TablesThickness.objects.all()
-
-
-class RacketsAttributeProduct:
-    def get_attribute_rackets(self):
-        return RacketsProductAttribute.objects.all()
-
-    def get_brand_rackets(self):
-        return RacketsBrand.objects.all()
-
-    def get_type_rackets(self):
-        return RacketsType.objects.all()
-
-    def get_handle_type_rackets(self):
-        return RacketsHandleType.objects.all()
-
-    def get_average_weight_rackets(self):
-        return RacketsAverageWeight.objects.all()
-
-    def get_rubbers_thickness_rackets(self):
-        return RacketsRubbersThickness.objects.all()
-
-
-class AccessoriesAttributeProduct:
-    def get_attribute_accessories(self):
-        return AccessoriesProductAttribute.objects.all()
-
-    def get_brand_accessories(self):
-        return AccessoriesBrand.objects.all()
-
-    def get_type_accessories(self):
-        return AccessoriesType.objects.all()
-
-    def get_color_accessories(self):
-        return AccessoriesColor.objects.all()
-
-
-class CategoryList(
-    BladesAttributeProduct,
-    RubbersAttributeProduct,
-    BallsAttributeProduct,
-    BackpacksBagsAttributeProduct,
-    NetsAttributeProduct,
-    TablesAttributeProduct,
-    RacketsAttributeProduct,
-    AccessoriesAttributeProduct,
-    ListView
-):
+class CategoryList(ListView):
     model = CategoryProduct
     template_name = 'store/home.html'
     context_object_name = 'categories_product'
@@ -178,36 +37,18 @@ class ProductDetailView(FormMixin, DetailView):
 
 
 def about_us(request):
-    list_phone = [
-        '+38(098)980-96-21'
-        '+38(098)980-96-22',
-        '+38(098)980-96-23',
-    ]
     data = {
-        'list_phone': list_phone,
     }
     return render(request, 'store/about.html', context=data)
 
 
 def ship_pay(request):
-    list_phone = [
-        '+38(098)980-96-21',
-        '+38(098)980-96-22',
-        '+38(098)980-96-23',
-    ]
     data = {
-        'list_phone': list_phone,
     }
     return render(request, 'store/ship_pay.html', context=data)
 
 
 def contacts(request):
-    list_phone = [
-        '+38(098)980-96-21',
-        '+38(098)980-96-22',
-        '+38(098)980-96-23',
-    ]
     data = {
-        'list_phone': list_phone,
     }
     return render(request, 'store/contacts.html', context=data)
